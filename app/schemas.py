5c794d3e425700b754aa2d15116631f6f72e4adb
Ayman Abdi Mohamed <9100aymanabdi@gmail.com>
2025-12-04 09:23:20 +0300

    added base schema for books, users, borrowedbooks, in schemas.py.

--- app/schemas.py
+++ app/schemas.py
@@ -1,12 +1,13 @@
-from pydantic import BaseModel
 
+from pydantic import BaseModel, EmailStr
+from typing import Optional
+import datetime
 
 
 # Base schema shared by all Genre-related schemas.
 # This defines the fields that a Genre must have when sent from the client.
 class GenreBase(BaseModel):
     name: str
-
 
 # Schema used when creating a new Genre (POST request).
 # Inherits all fields from GenreBase.
@@ -22,6 +23,7 @@ class Genre(GenreBase):
 class BookBase(BaseModel):
     title: str
     author: str
+    pages: Optional[int] = None
     genre_id: int
 
 class BookCreate(BookBase):
@@ -29,5 +31,27 @@ class BookCreate(BookBase):
 
 class Book(BookBase):
     id: int
+    genre: Optional[Genre] = None
     class Config:
         orm_mode = True
+
+class UserBase(BaseModel):
+    name: str
+    email: EmailStr
+
+class UserCreate(UserBase):
+    pass
+
+class User(UserBase):
+    id: int
+    class Config:
+        orm_mode = True
+
+class BorrowedBase(BaseModel):
+    user_id: int
+    book_id: int
+    borrowed_date: Optional[datetime.date] = None
+    return_date: Optional[datetime.date] = None
+
+class BorrowedCreate(BorrowedBase):
+    pass
