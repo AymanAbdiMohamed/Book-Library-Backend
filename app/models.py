622a4fb57377cbd5e8383c9487f870a307f19bc7
Ayman Abdi Mohamed <9100aymanabdi@gmail.com>
2025-12-04 08:16:06 +0300

    added Book class in models.py

--- app/models.py
+++ app/models.py
@@ -6,9 +6,17 @@ class Genre(Base):
     __tablename__ = "genres"
 
     id = Column(Integer, primary_key=True, index=True)
-    name = Column(String, unique=True, index=True)
+    name = Column(String, unique=True)
 
     books = relationship("Book", back_populates="genre")
 
 
+class Book(Base):
+    __tablename__ = "books"
 
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String)
+    author = Column(String)
+    genre_id = Column(Integer, ForeignKey("genres.id"))
+
+    genre = relationship("Genre", back_populates="books")
