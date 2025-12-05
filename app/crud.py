69b0a80459fb2b9481436374d15b06076b9a33e7
Ayman Abdi Mohamed <9100aymanabdi@gmail.com>
2025-12-05 14:47:17 +0300

    created a new book entry for books get in crud.py

--- app/crud.py
+++ app/crud.py
@@ -10,7 +10,24 @@ def get_genres(db: Session):
 def create_genre(db: Session, genre: schemas.GenreCreate):
     # create a new genre using data from GenreCreate schema
     db_genre = models.Genre(name=genre.name)
-    db.add(db_genre)
-    db.commit()
-    db.refresh(db_genre)
+    db.add(db_genre) # add new genre object to the database session
+    db.commit() # commit transaction to save to DB
+    db.refresh(db_genre) # refresh object with DB-generated fields; such as id
     return db_genre
+
+#Books
+def get_books(db: Session):
+    return db.query(models.Books).all()
+
+de create_book(db: Session, book: schemas.BookCreate):
+    # create a new book entry
+    db_book = models.Boook(
+        title=book.title,
+        author=book.author,
+        pages=book.pages,
+        genre_id=book.genre_id
+    )
+    db.add(db_book)
+    db.commit()
+    db.refresh(db_book)
+    return db_book
